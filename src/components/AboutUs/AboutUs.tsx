4b603fcc949b4ae01835e0139c61689b980b0779
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-09 02:49:55 +0100

    Importing style for About component

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,15 +1,25 @@
 // - Constants -
 import { backgroundImage } from "../../constants/ImagesConstants";
 import aboutUsData from "../../constants/AboutConstant";
+// - Icons -
+import { FaHome } from "react-icons/fa";
+// - React -
+import { Link } from 'react-router-dom';
 
 const AboutUs = () => {
   return (
-    <div className="h-screen bg-cover bg-center flex justify-center items-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="flex flex-col justify-center items-center mx-auto py-[2.5rem] bg-white xl:w-[50%] lg:w-[75%] w-[90%] pt-[4rem]">
-        <h1 className="text-[3rem] font-bold text-center mt-0 mb-[1.5rem] font-workSans">{aboutUsData.title}</h1>
+    <div className="min-h-screen bg-cover bg-center flex items-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="flex flex-col mx-auto py-[2.5rem] rounded-[1rem] rounded-br-none hover:rounded-br-[1rem] ease-in-out duration-300 bg-white hover:bg-opacity-90 xl:w-[50%] lg:w-[75%] w-[90%] md:pt-[4rem]">
+        <div className="text-[3rem] font-bold text-center mt-0 mb-[1.5rem] font-workSans">{aboutUsData.title}</div>
         <div className="w-[90%] mx-auto text-[1.2rem] text-black font-montserrat">
           {aboutUsData.content}
         </div>
+        <Link to="/">
+            <div className="flex items-center pl-[2.5rem] pt-[1rem] cursor-pointer group">
+            <FaHome className="w-[2rem] h-[2rem] transition-all duration-300 ease-in-out mr-[0.5rem]" />
+            <div className="opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 font-montserrat z-10">Back to Home</div>
+            </div>
+        </Link>
       </div>
     </div>
   );
